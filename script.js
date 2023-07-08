a5c31576c3c210abe2b3395f690f24a9a4b38b92
Sarthak kundra <sarthakkundra21@gmail.com>
2023-07-08 18:30:47 +0530

    fixed ankits bs

---
+++ script.js
@@ -0,0 +1,8 @@
+// const renderAnimationWithDelay = () => {
+    // const titleEl = document.getElementById("title");
+    // titleEl.classList.add("focus-in-expand");
+// }
+window.addEventListener("load", (e) => {
+    const titleEl = document.getElementById("title");
+    titleEl.classList.add("focus-in-expand");
+})
